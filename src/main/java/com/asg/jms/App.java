1d7c80733196757b32ed97f0a3dc9e23aa28035b
qzhang16 <qzhang16@ra.rockwell.com>
2023-08-14 11:41:17 +0800

    connection.start is required for consuming messages, but not for sending messages.

--- src/main/java/com/asg/jms/App.java
+++ src/main/java/com/asg/jms/App.java
@@ -1,29 +1,51 @@
 package com.asg.jms;
 
-import javax.jms.JMSContext;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
-
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        System.out.println("Hello World!");
-        
-        InitialContext context = new InitialContext();
-		Queue queue = (Queue) context.lookup("queue/queue01");
-		
-		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
-				JMSContext jmsContext = cf.createContext()){
-			jmsContext.createProducer().send(queue,"Arise Awake and stop not till the goal is reached");
-			String messageReceived = jmsContext.createConsumer(queue).receiveBody(String.class);
-			System.out.println(messageReceived);
-		}
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World!");
+
+		InitialContext context = new InitialContext();
+		// Queue queue = (Queue) context.lookup("queue/queue01");
+
+		// try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+		// JMSContext jmsContext = cf.createContext()){
+		// jmsContext.createProducer().send(queue,"Arise Awake and stop not till the
+		// goal is reached");
+		// String messageReceived =
+		// jmsContext.createConsumer(queue).receiveBody(String.class);
+		// System.out.println(messageReceived);
+		// }
+		ConnectionFactory cf = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = cf.createConnection("admin", "admin");
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Queue destination = (Queue) context.lookup("queue/queue01");
+
+		MessageProducer proceducer = session.createProducer(destination);
+		TextMessage msg = session.createTextMessage("I am the creator of my	destination");
+		proceducer.send(msg);
+
+		proceducer.close();
+		MessageConsumer consumer = session.createConsumer(destination);
+		connection.start();
+		TextMessage msg01 = (TextMessage) consumer.receive(100);
+		if (msg01 != null) System.out.println(msg01.getText());
+
+		consumer.close();
+		session.close();
+		connection.close();
+
+	}
 }
